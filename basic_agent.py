3b314df1cad7344cee4854824ab3ca7c237476aa
Koushik Karan Geetha Nagaraj <koushikkaran6@gmail.com>
2024-02-12 09:12:18 -0500

    fixed main

---
+++ basic_agent.py
@@ -0,0 +1,120 @@
+
+import pygame
+import numpy as np
+from controllers import *
+# from test_cases import *
+from loop_agents import *
+from Environment import Environment
+
+class CarSimulation(Environment):
+    def __init__(self, start_vec, goal_vec, obstacle_vec, controller):
+        super().__init__(800, 800, obstacle_vec)
+        # Initialize Pygame necessary for initialising the simulation window and graphics
+        print('Initializing Agents')
+        pygame.init()
+
+        pygame.display.set_caption("Car Simulation")#Windows heading
+
+        self.debugging = False
+        self.save_data = True
+
+        # Set up car and goal positions
+        self.car_pos = start_vec
+        self.goal_pos = goal_vec
+        self.update_poses(start_vec, goal_vec)
+        self.obstacles = obstacle_vec
+        self.controller = controller
+        self.clock = pygame.time.Clock()
+        self.frame_rate= 60
+
+        self.control = self.controller(self.car_pos, self.goal_pos, self.obstacles)
+        self.control.dt = 1/self.frame_rate
+        self.infinity = LoopSimulation(800,800,120,1,1)
+        
+        # self.control.create_agents(new_agents)
+        self.timer=0
+        self.car_pos = self.control.x
+        self.goal_pos = self.control.goal_pos
+    
+
+    def run_simulation(self):
+        print('running')
+        # Main simulation loop
+        running = True
+        while running:
+            #checks if window is closed and closes the loop by setting False
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    running = False
+
+           # Update car position
+            if self.timer%100==0:
+                new_agents=self.infinity.run_simulation(self.car_pos,self.goal_pos)
+                self.control.create_agents(new_agents)
+                self.car_pos = self.control.x
+                self.goal_pos = self.control.goal_pos
+
+            self.car_pos,self.goal_pos = self.control.car_pos()
+            self.update_poses(self.car_pos, self.goal_pos)
+            self.intersections=self.control.intersection()
+            self.draw_map('white') # draws map with obstacles
+            
+            self.draw_agents_with_goals(self.control.agent_collision) # draws agents and their respective goal positions
+            speed=self.control.traffic_speed()
+            collision_rate,total_time=self.control.collison_rate()
+            capacity,volume=self.control.volume_capacity()
+           
+            #metrics
+            if self.debugging == True:
+                self.display_collision_rate(collision_rate)
+                self.display_total_time(total_time)
+                self.display_v_c_ratio(volume,capacity)
+                self.display_traffic_speed(speed)
+
+            # setting the segment
+            twin_boxes = np.array([[-60,300,400,300],[-60,450,50,300]])
+            self.frame_angle,centers,(t_l,t_r,b_l,b_r)=self.segment_frame(twin_boxes)
+            side_length = twin_boxes[:,-1]
+            # plotting the segment
+            #local points of all self.x
+            global_points=self.control.x[:,:2]
+            local_points = self.global_local_transform(global_points,t_l,self.frame_angle, )
+            
+            if self.debugging:
+                self.plot_segment_frame(centers,(t_l,t_r,b_l,b_r))
+
+            if self.save_data:
+                
+                #getting the agents in the frame
+                # camera_x_local,camera_x_global=self.camera_agents(local_points,side_length, self.control.x) #####uncomment
+                # print(camera_x_local,len(camera_x_local))
+                #saving camera1 dataset
+                self.save_camera_image(side_length,(t_l,t_r,b_l,b_r),self.timer, 6000, 1500, 1500)
+                #saving camera csv file
+                # self.save_camera_data(self.timer,camera_x_local,camera_x_global)
+
+            pygame.display.update()
+            self.clock.tick(self.frame_rate)
+            self.timer+=1
+
+            
+        pygame.quit()
+
+if __name__ == "__main__":
+   
+    start = np.array([[-50.0, 300.0, 0.0, 0.0,1],[-30.0, 50.0, 0.0, 0.0,1]])
+    goal = np.array([[800, 1500],[700, 1600]])   
+    obs = {'circle': np.array([]),
+       'rectangle': np.array([[500,500,30,30],
+                              [700,700,30,30],
+                              [300,300,30,30],
+                              [700,300,30,30],
+                              [300,700,30,30],
+                              [300,500,30,30],
+                              [500,300,30,30],
+                              [700,500,30,30],
+                              [500,700,30,30]])}
+
+
+    simulation = CarSimulation(start, goal, obs, DoubleIntegrator)
+    simulation.run_simulation()
---
+++ loop_agents.py
@@ -0,0 +1,197 @@
+import pygame
+import random
+import numpy as np
+from controllers import *
+from basic_agent import *
+from itertools import permutations
+
+class LoopSimulation:
+    def __init__(self, frame_h, frame_w, dist2GE_min, obs, controller):
+        """
+        Initializes simulation that ends after some time period
+
+        Args:
+            frame_h (int): height of visible frame. Agents will spawn beyond this.
+            frame_w (int): width of visible frame. Agents will spawn beyond this.
+            dist2GE_min (int or float): min distance from start/goal pos to edge of frame.
+            dist2GE_max (int or float):  maxdistance from start/goal pos to edge of frame.
+            obs (dict): obstacle vector with format {"circle": [[x1,y1]], [...]],
+                                                     "rectangle": [[x1,y1,w1,h1], [...]]}
+        """
+
+        self.frame_h = frame_h
+        self.frame_w = frame_w
+        self.dist2GE_min = dist2GE_min
+        
+        # Initialize Pygame necessary for initialising the simulation window and graphics
+        print('Initializing')
+        pygame.init()
+
+        # Set up car and goal positions
+        self.obstacles = obs
+        self.controller = controller
+        self.clock = pygame.time.Clock()
+        self.frame_rate = 60
+
+        self.agents = {"start" : [], "goal" : []}
+
+    def run_simulation(self,old_agents_start=None,old_agents_goal=None):
+        self.old_agents_start=old_agents_start
+        self.old_agents_goal=old_agents_goal
+        start_points,goal_points=self.create_agents(1, 50)
+        new_agents = {"start": start_points, "goal" :goal_points}
+        return new_agents
+    
+    def probability_agent_spawning(self,probability, size=1):
+        """
+    Generate random binary values (1 or 0) based on a given probability.
+
+    Parameters:
+    - probability: Probability of generating 1 (should be between 0 and 1).
+    - size: Number of random values to generate (default is 1).
+
+    Returns:
+    - numpy array of generated binary values.
+    """
+        random_values = np.random.rand(size)  # Generate random values between 0 and 1
+        binary_values = (random_values < probability).astype(int)  # Convert to 1 or 0 based on the probability
+        indices_ones = np.where(binary_values == 1)
+        return len(indices_ones[0])
+    
+    def bool_check_distance_spawn_point(self,distance_matrix,distance_threshold=25):
+        #This function takes the matrix and returns the bool of the points within the threshold
+        a_g_dist = np.squeeze(distance_matrix, axis=-1)
+        a_g_dist = np.transpose(a_g_dist, ( 1, 0))
+        a_g_dist_bool= np.any(a_g_dist < distance_threshold, axis=0)
+        return a_g_dist_bool
+    
+    def check_spawning_overlap(self,start_points,goal_points):
+        #This core function checks if the new points overlap with any points of start and goal
+        if  self.old_agents_start is not None   and self.old_agents_goal is not None:
+            start_start_diff = start_points[:,:2][:,np.newaxis] - self.old_agents_start[:,:2][np.newaxis,:,:]
+            start_start_dist = np.linalg.norm(start_start_diff, axis = 2)[:,:,np.newaxis] + (1e-6)
+            start_goal_diff = start_points[:,:2][:,np.newaxis] - self.old_agents_goal[np.newaxis,:,:]
+            start_goal_dist = np.linalg.norm(start_goal_diff, axis = 2)[:,:,np.newaxis] + (1e-6)
+
+            new_start_new_start_diff=start_points[:,:2][:,np.newaxis] - start_points[:,:2][np.newaxis,:,:]
+            new_start_new_start_dist = np.linalg.norm(new_start_new_start_diff, axis = 2)[:,:,np.newaxis] + (1e-6)
+
+            new_start_new_start_dist = np.squeeze(new_start_new_start_dist, axis=-1)
+            np.fill_diagonal(new_start_new_start_dist, np.inf)
+            new_start_new_start_dist=new_start_new_start_dist[:,:,np.newaxis]
+
+            new_start_new_goal_diff=start_points[:,:2][:,np.newaxis] - goal_points[np.newaxis,:,:]
+            new_start_new_goal_dist = np.linalg.norm(new_start_new_goal_diff, axis = 2)[:,:,np.newaxis] + (1e-6)
+            new_goal_new_goal_diff=goal_points[:,np.newaxis] - goal_points[np.newaxis,:,:]
+            new_goal_new_goal_dist = np.linalg.norm(new_goal_new_goal_diff, axis = 2)[:,:,np.newaxis] + (1e-6)
+            new_goal_new_goal_dist = np.squeeze(new_goal_new_goal_dist, axis=-1)
+            np.fill_diagonal(new_goal_new_goal_dist, np.inf)
+            new_goal_new_goal_dist=new_goal_new_goal_dist[:,:,np.newaxis]
+
+            new_goal_new_start_diff=goal_points[np.newaxis,:,:]-start_points[:,:2][:,np.newaxis] 
+            new_goal_new_start_dist = np.linalg.norm(new_goal_new_start_diff, axis = 2)[:,:,np.newaxis] + (1e-6)
+            goal_start_diff =  goal_points[:,np.newaxis] -self.old_agents_start[:,:2][np.newaxis,:,:]
+            goal_start_dist = np.linalg.norm(goal_start_diff, axis = 2)[:,:,np.newaxis] + (1e-6)
+            goal_goal_diff =   goal_points[:,np.newaxis]-self.old_agents_goal[np.newaxis,:,:]
+            goal_goal_dist = np.linalg.norm(goal_goal_diff, axis = 2)[:,:,np.newaxis] + (1e-6)
+
+            s_s_bool=self.bool_check_distance_spawn_point(start_start_dist)
+            s_g_bool=self.bool_check_distance_spawn_point(start_goal_dist)
+            g_s_bool=self.bool_check_distance_spawn_point(goal_start_dist)
+            g_g_bool=self.bool_check_distance_spawn_point(goal_goal_dist)
+            new_s_s_bool=self.bool_check_distance_spawn_point(new_start_new_start_dist)
+            new_s_g_bool=self.bool_check_distance_spawn_point(new_start_new_goal_dist)
+            new_g_g_bool=self.bool_check_distance_spawn_point(new_goal_new_goal_dist)
+            new_g_s_bool=self.bool_check_distance_spawn_point(new_goal_new_start_dist)
+
+            validate_points= np.logical_or.reduce([s_s_bool,s_g_bool,g_s_bool,g_g_bool,new_s_g_bool,new_s_s_bool,new_g_g_bool,new_g_s_bool])
+            validate_points_collide_indices=np.where(validate_points)
+            mask = np.ones(start_points.shape[0], dtype=bool)
+            mask[validate_points_collide_indices] = False
+            start_points=start_points[mask]
+            goal_points=goal_points[mask]
+
+        return start_points,goal_points,len(start_points)
+
+    def create_agents(self, p, n):
+        """
+        creates start and goal points for agents to spawn in
+
+        Args:
+            p (float): probability with which to spawn agent(s).
+            n (int): number of agents to spawn with probability p (See max_agents_flag).
+            max_agents_flag (bool): if True n is the max number of agents to spawn. else
+                                    n in the total number of agents spawned with the 
+                                    probability p 
+
+        Returns:
+            start (np.ndarray): start vector for agents.
+            goal (np.ndarray)
+        """
+
+        n=self.probability_agent_spawning(p,n)
+        sample_n=n*2
+        # start_points_res,goal_points_res=np.empty((0, 4)),np.empty((0, 2))
+        while(n!=0):
+            
+            AgentR = ["R","L","U","D"]
+            pairs_permutations = list(permutations(AgentR, 2))
+            start_goal_random= np.array(random.choices(pairs_permutations, k=sample_n))
+
+            start_random_var = start_goal_random[:, 0]
+            goal_random_var = start_goal_random[:, 1]
+
+            start_points = np.zeros((sample_n,4), dtype=float)
+            goal_points= np.zeros((sample_n,2), dtype=float)
+
+            for case in AgentR:
+                condition_start = np.where(start_random_var == case)[0]
+                condition_goal = np.where(goal_random_var == case)[0]
+
+                if case=="L":
+                    x_start = np.random.sample(len(condition_start))*(self.dist2GE_min - 20) - self.dist2GE_min
+                    y_start=np.random.sample(len(condition_start))*(self.frame_h+(self.dist2GE_min))- self.dist2GE_min
+                    x_goal = np.random.sample(len(condition_goal))*(self.dist2GE_min - 20) - self.dist2GE_min
+                    y_goal=np.random.sample(len(condition_goal))*(self.frame_h+(self.dist2GE_min))- self.dist2GE_min
+                
+                elif case=="U":
+                    y_start = np.random.sample(len(condition_start))*(self.dist2GE_min - 20) - self.dist2GE_min
+                    x_start=np.random.sample(len(condition_start))*(self.frame_h+(self.dist2GE_min))- self.dist2GE_min
+                    y_goal = np.random.sample(len(condition_goal))*(self.dist2GE_min - 20) - self.dist2GE_min
+                    x_goal=np.random.sample(len(condition_goal))*(self.frame_h+(self.dist2GE_min))- self.dist2GE_min
+                
+                elif case=="R":
+                    x_start = np.random.sample(len(condition_start))*(self.dist2GE_min )+(self.frame_w+20 ) 
+                    y_start= np.random.sample(len(condition_start))*(self.frame_h+(self.dist2GE_min))- self.dist2GE_min
+                    x_goal = np.random.sample(len(condition_goal))*(self.dist2GE_min )+(self.frame_w+20 ) 
+                    y_goal= np.random.sample(len(condition_goal))*(self.frame_h+(self.dist2GE_min))- self.dist2GE_min
+
+                else:
+                # case=="D":
+                    y_start = np.random.sample(len(condition_start))*(self.dist2GE_min )+(self.frame_h+20 ) 
+                    x_start = np.random.sample(len(condition_start))*(self.frame_w+(self.dist2GE_min))- self.dist2GE_min
+                    y_goal = np.random.sample(len(condition_goal))*(self.dist2GE_min )+(self.frame_h+20 ) 
+                    x_goal = np.random.sample(len(condition_goal))*(self.frame_w+(self.dist2GE_min))- self.dist2GE_min
+                    
+                start_points[condition_start,0]=x_start
+                start_points[condition_start,1]=y_start
+                goal_points[condition_goal,0]=x_goal
+                goal_points[condition_goal,1]=y_goal
+
+            color_vec = np.random.randint(0,6,size=(start_points.shape[0],1))
+            start_points = np.hstack([start_points,color_vec])
+            # print(start_points[0])
+
+            start_points,goal_points,sample_n=self.check_spawning_overlap(start_points,goal_points)
+            # print("ahm",start_points.shape,goal_points.shape,"sample",sample_n,"n",n)
+            if sample_n>n:
+                start_points=start_points[:n]
+                goal_points=goal_points[:n]
+            n=0
+            # else:
+            #     n=sample_n=n-sample_n
+            #     start_points_res,goal_points_res=np.vstack([start_points_res,start_points]),np.vstack([goal_points_res,goal_points])
+            #     self.old_agents_start,self.old_agents_goal=np.vstack([self.old_agents_start,start_points]),np.vstack([self.old_agents_goal,goal_points])
+            # print(n,sample_n)
+        return start_points,goal_points
+    
